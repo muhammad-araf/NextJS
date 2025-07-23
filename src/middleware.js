517720c46411fb8327f4aecfbc61f3a94d5987c6
muhammad-araf <lentoaraf@gmail.com>
2025-07-24 02:54:41 +0500

    Middleware in Next JS Routing

---
+++ src/middleware.js
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+const isLogin = false
+export function middleware(req){
+    if(!isLogin && req.nextUrl.pathname!='/employee'){
+        return NextResponse.redirect(new URL('/employee',req.url));
+    }
+}
