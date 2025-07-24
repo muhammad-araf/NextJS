d90f582927ad97afb76bf8fbaaba3985fd6d1d7f
muhammad-araf <lentoaraf@gmail.com>
2025-07-24 23:04:34 +0500

    Config Middle ware

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,10 @@
 import { NextResponse } from "next/server";
-const isLogin = false
+// const isLogin = false
 export function middleware(req){
-    if(!isLogin && req.nextUrl.pathname!='/employee'){
+    // if(!isLogin && req.nextUrl.pathname!='/employee'){
         return NextResponse.redirect(new URL('/employee',req.url));
-    }
+    // }
+}
+export const config = {
+    matcher: ['/Segment/:posts*']
 }
