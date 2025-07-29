c1a999d328d0393c2ad3d0f77cff5353cc83fb17
muhammad-araf <lentoaraf@gmail.com>
2025-07-29 22:05:23 +0500

    Connect and Send data with text field and button to MONGO Atlas Data-Base

---
+++ src/app/todatabase/page.js
@@ -0,0 +1,26 @@
+'use client'
+import React, { useState } from 'react'
+
+const page = () => {
+    const [name,setName] = useState('');
+    const [age,setAge] = useState('');
+    const handleData = async () => {
+        let data = await fetch('http://localhost:3000/api/db-env',{
+            method : 'POST',
+            body : JSON.stringify({name,age})
+        })
+        data = await data.json();
+        console.log(data)
+
+    }
+  return (
+    <>
+    <input type='text' placeholder='Enter your name' value={name} onChange={(e)=>setName(e.target.value)}/><br/><br/>
+    <input type='Number' placeholder='Enter your Age' value={age} onChange={(e)=>setAge(e.target.value)}/><br/><br/>
+    <button onClick={handleData}>Update Data</button>
+    
+    </>
+  )
+}
+
+export default page
