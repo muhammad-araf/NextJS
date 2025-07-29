2db0bebb64b8054e85ade6be3ec13813bf7d2eaa
muhammad-araf <lentoaraf@gmail.com>
2025-07-29 23:04:41 +0500

    POST DATA to mongo Atlas

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      
+      <Link href={'/manipulatedata/send'}>Send Data</Link><br/>
+
+      <Link href={'/manipulatedata/get'}>Get Data</Link>
     </div>
   );
 }
